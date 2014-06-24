8ba5fc38ed54e2a3eb8eacc873c6e69c72d1dadf
Vanessa Anderson <Vanessa.Anderson@teachforamerica.org>
2014-06-24 18:50:37 -0400

    Add in push notification code and code signing

--- Spothauc/Spothauc/AppDelegate.m
+++ Spothauc/Spothauc/AppDelegate.m
@@ -24,6 +24,9 @@
     
     [PFFacebookUtils initializeFacebook];
     
+    [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|
+     UIRemoteNotificationTypeAlert|
+     UIRemoteNotificationTypeSound];
    
     return YES;
 }
@@ -57,6 +60,20 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation saveInBackground];
+}
+
+- (void)application:(UIApplication *)application
+didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [PFPush handlePush:userInfo];
 }
 
 @end
